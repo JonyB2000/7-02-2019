c16892a328d8561d572ce518b652c9c311bdcfa7
JonyB2000 <47424434+JonyB2000@users.noreply.github.com>
2019-02-07 09:26:47 -0600

    Create Reto_3.js

---
+++ Reto_3.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(255);
+	fill(255,0,0);
+	stroke(255,0,0);
+	ellipse(200,200,285,285);
+	fill(255);
+	ellipse(200,200,280,280);
+	strokeWeight(20);
+	line(100,100,300,300);
+}
